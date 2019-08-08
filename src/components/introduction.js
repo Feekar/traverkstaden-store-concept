465b532c77bc4f4390b09a25cb4c5dd6f224abb0
Jonas Gustavsson <jonasgson@outlook.com>
2019-08-08 20:09:38 +0200

    .

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+// import { graphql, useStaticQuery } from 'gatsby';
 
 const Introduction = () => {
-  const data = useStaticQuery(graphql`
-  query {
-    allContentYaml(
-      filter: {
-        introduction: {text: {ne: null}}
-      }
-    ) {
-      edges {
-        node {
-          introduction{
-            text
-            image          
-          }
-        }
-      }
-    }
-  }`);
+  // const data = useStaticQuery(graphql`
+  // query {
+  //   allContentYaml(
+  //     filter: {
+  //       introduction: {text: {ne: null}}
+  //     }
+  //   ) {
+  //     edges {
+  //       node {
+  //         introduction{
+  //           text
+  //           image
+  //         }
+  //       }
+  //     }
+  //   }
+  // }`);
+  const test = 2;
+  console.log(test);
 
-  const content = data.allContentYaml.edges[0].node.introduction;
+  // const content = data.allContentYaml.edges[0].node.introduction;
 
-  return <span>{content.text}</span>;
+  return <span>test</span>;
 };
-
 
 export default Introduction;
