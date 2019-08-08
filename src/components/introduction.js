b02a63cf834cbc19306296ba02f6deb1bd99347c
Jonas Gustavsson <jonasgson@outlook.com>
2019-08-08 22:11:46 +0200

    ,.

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -2,28 +2,28 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
 const Introduction = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allFile(filter: { name: { eq: "introduction" } }) {
-        edges {
-          node {
-            childMarkdownRemark {
-              frontmatter {
-                introduction {
-                  text
-                  image
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     allFile(filter: { name: { eq: "introduction" } }) {
+  //       edges {
+  //         node {
+  //           childMarkdownRemark {
+  //             frontmatter {
+  //               introduction {
+  //                 text
+  //                 image
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
 
-  const content = data.allFile.edges[0].node.childMarkdownRemark.frontmatter;
+  // const content = data.allFile.edges[0].node.childMarkdownRemark.frontmatter;
 
-  return <span>{content.text}</span>;
+  return <span>test</span>;
 };
 
 export default Introduction;
