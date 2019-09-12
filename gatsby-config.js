816a6531cfed25fe0c3643a8daae9099e14e37b7
Jonas Gustavsson <jonasgson@outlook.com>
2019-09-12 14:22:47 +0200

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,11 +13,12 @@ module.exports = {
     'gatsby-plugin-sass',
     'gatsby-transformer-sharp',
      {
-      resolve: `gatsby-plugin-netlify-cms`,
+      resolve: 'gatsby-plugin-netlify-cms',
       options: {
         enableIdentityWidget: true,
       },
     },
+    'gatsby-plugin-netlify-identity-widget',
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-prefetch-google-fonts',
